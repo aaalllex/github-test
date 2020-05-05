02725b970341452ac04019898045fbb3d24faedc
aaalllex <alexshlayfer@gmail.com>
2020-05-05 22:46:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     # canvas = tk.Canvas(root, width=640, height=440, bg='#6872f4')
     # canvas.pack(side=tk.LEFT)
 
-    lable1 = tk.Label(root, text="branch2 remote ", justify='center')
+    lable1 = tk.Label(root, text="branch2 remote br5 ", justify='center')
     lable1.pack()
 
 
